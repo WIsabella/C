20e1944034f397403f080fedf3ba191e1773ba4f
smile-sdu-wjl <1849709067@qq.com>
2023-11-20 09:50:04 +0800

    daily

--- ojqus003.c
+++ ojqus003.c
@@ -8,19 +8,36 @@ int main()
     int n=0,key=0;
     scanf("%d",&n);
     int Array[n];
-    scanf("%d",key);
+    scanf("%d",&key);
     for(int i=0;i<n;i++)
     {
-        scanf("%d",Array[i]);
+        scanf("%d",&Array[i]);
     }
-    int start=1;
-    int end =n;
-    do{
+    int start=0;
+    int end =n-1;
+    int middle=0;
+    while(start<=end)
+    {
+        middle=(start+end)/2;
+        if(Array[middle]==key)
+        {
+            printf("%d",middle);
+            break;
+        }
+        else if(Array[middle]>key)
+        {
+            end=middle-1;
+        }
+        else 
+        start=middle+1;
+        if(start>=end-1&&Array[start]!=key&&Array[end]!=key)
+        {
+            printf("-1");
+            break;
 
-
+        }
         
     }
-
 
 
 }
---
+++ ojqus005.c
@@ -0,0 +1,37 @@
+//利用递归实现。在一组有 n 个元素的有序数据中折半查找关键字 key，如果找到，返回 key 所在的位置，否则返回-1。
+//第一行参数 N，M；N 为这一组数的个数，M 为待查找的数；第二行为 N 个整数，为排序后的数。
+//一个整数 J，若能找到，这该整数在数组内的下标，若无法找到，则输出-1；
+
+#include<stdio.h>
+int tSearch(int start,int end,int M,int Array[])
+{
+    if(start>end)
+    return -1;
+    int middle=(start+end)/2;
+    if(Array[middle]==M)
+    return middle;
+    else if(Array[middle]>M)
+    return tSearch(middle+1,end,M,Array);
+    else if(Array[middle]<M)
+    return tSearch(start,middle-1,M,Array);
+
+}
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    int start=0;
+    int end=N-1;
+    int M=0;
+    scanf("%d",&M);
+    int Array[N];
+    for(int i=0;i<N;i++)
+    {
+        scanf("%d",&Array[i]);
+    }
+    int F=tSearch(start,end,M,Array);
+    printf("%d",F);
+
+
+
+}

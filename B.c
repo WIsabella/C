23ab395b78ec9a3c37afa3a10ce2bcc45007a931
smile-sdu-wjl <1849709067@qq.com>
2023-11-27 08:03:10 +0800

    daily

--- B.c
+++ B.c
@@ -1,16 +1,18 @@
-#include "stdio.h"
+#include<stdio.h>
 
 // 二分查找
-int search(int *arr, int key, int length) {
-  int head = 0, tail = length - 1, mid;
-  while (head <= tail) {
-    mid = (head + tail) / 2;
+int search(int *arr, int key, int length)
+{
+  int start = 0, end = length - 1, mid;
+  while (start <= end)
+  {
+    mid = (start + end) / 2;
     if (key == arr[mid])
       return mid;
     else if (key > arr[mid])
-      head = mid + 1;
+      start = mid + 1;
     else
-      tail = mid - 1;
+      end = mid - 1;
   }
   return -1;
 }
@@ -20,13 +22,16 @@ int main() {
   scanf("%d %d", &n, &m);
   int arr[n];
   int target[m];
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; ++i)
+  {
     scanf("%d", &arr[i]);
   }
-  for (int i = 0; i < m; ++i) {
+  for (int i = 0; i < m; ++i)
+  {
     scanf("%d", &target[i]);
   }
-  for (int i = 0; i < m; ++i) {
+  for (int i = 0; i < m; ++i)
+  {
     printf("%d\n", search(arr, target[i], n));
   }
   return 0;
---
+++ learnB.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int Search(int A[],int key,int length)
+{
+    int start=0;
+    int end=length-1;
+    int mid;
+    while(start<=end)
+    {
+        mid=(start+end)/2;
+        if(A[mid]==key)
+        return mid;
+        else if(A[mid]<key)
+        {
+            start=mid+1;
+        }
+        else if(A[mid]>key)
+        {
+            end =mid-1;
+        }
+    }
+    return -1;
+
+} 
+int main()
+{
+    int n=0;
+    scanf("%d",&n);
+    int m;
+    scanf("%d",&m);
+    int A[n];
+    int B[m];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&A[i]);
+    }
+    for(int i=0;i<m;i++)
+    {
+        scanf("%d",&B[i]);
+    }
+    for(int i=0;i<m;i++)
+    {
+        printf("%d\n",Search(A,B[i],m));
+    }
+
+
+}

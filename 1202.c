717e52d756e1ea3103f98a509ad5d2b3b37f6b34
smile-sdu-wjl <1849709067@qq.com>
2023-12-04 15:40:29 +0800

    daily

---
+++ 1202.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int A[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&A[i]);
+    }
+    int m;
+    scanf("%d",&m);
+    int j;
+    A[-1]=-1;
+    A[n]=1000;//wok,Ì«ÉñÆæÁË°É£¡
+    for(j=0;j<n-1;j++)
+    {
+        if(A[j-1]<m&&A[j]>m)
+        {
+            break;
+        }
+    }
+    for(int l=n;l>j;l--)
+    {
+        A[l]=A[l-1];
+    }
+    A[j]=m;
+    for(int i=0;i<=n;i++)
+    {
+    printf("%d ",A[i]);
+    }
+
+}

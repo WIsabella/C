836372cc83cb5a791eac7bca597a12f60893eda5
smile-sdu-wjl <1849709067@qq.com>
2023-12-11 09:26:39 +0800

    daily

--- ojqus0011.c
+++ ojqus0011.c
@@ -11,20 +11,24 @@ void f(int D,int B)
         if(n<10)
         printf("%d",n);
         else
-        printf("%c",n-9+96);
-
+        printf("%c",n+87);
     }
 }
 int main()
 {
     int A,B;
-    char C[10];
+    char C[11];
     int D=0;
     scanf("%d %d",&A,&B);
     scanf("%s",C);
     int n=strlen(C);
     for(int i=0;i<n;i++)
     {
+        if(A==16&&C[i]>=10)
+        {
+            D+=(C[i]-87)*pow(A,n-1-i);
+        }
+        else
         D+=(C[i]-48)*pow(A,(n-1-i));//储存的是ASCII码   减去48才是真正的数据！！！！
     }
     // do

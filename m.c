8f8671fc10cc600dbed07b466c401188a22cd5dd
smile-sdu-wjl <1849709067@qq.com>
2023-12-28 15:30:06 +0800

    daily

---
+++ m.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<math.h>
+int f(int n);
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int max=0;
+    for(int i=0;i<n;i++)
+    {
+        if(f(a[i])>max)
+        max=a[i];
+    }
+    printf("%d",max);
+}
+int f(int n)
+{
+    int flag=1;
+    a=sqrt
+    if(flag==1)return n;
+    else
+    return 0;
+}

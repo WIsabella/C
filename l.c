83fc37d7cdf926866799b057e8f98762601d6efa
smile-sdu-wjl <1849709067@qq.com>
2023-12-30 16:05:55 +0800

    daily

--- l.c
+++ l.c
@@ -1,5 +1,26 @@
-#include<stdio.h>
-#include<math.h>
-int main()
+void Print_Factorial(const int N)
 {
+    int n = N;
+    if (n < 0)
+        printf("Invalid input");
+    else
+    {
+        int a[3000] = { 1,0 }, index = 1, carry = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            for (int j = 0; j < index; j++)
+            {
+                int temp = a[j] * i + carry;
+                a[j] = temp % 10;
+                carry = temp / 10;
+            }
+            while (carry)
+            {
+                a[index++] = carry % 10;
+                carry /= 10;
+            }
+        }
+        for (int i = index - 1; i >= 0; i--)
+            printf("%d", a[i]);
+    }   
 }

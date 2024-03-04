50ce4ddea7370c6a959d90897d8f427e380f056b
smile-sdu-wjl <1849709067@qq.com>
2024-03-04 19:18:21 +0800

    daily

--- 0112.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    cout<<"Hello World!"<<"我爱你啊"<<"\n";
-    return 0;
-}
--- lab1_1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-using namespace std;
-int main()
-{
-    cout<<"Hello\n";
-    cout <<"WANG Junlei!\n"<<endl;
-    return 0;
-}
